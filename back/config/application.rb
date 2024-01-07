adc2747b4fe3cdff8a20f11589e88f7ca4f19015
rika <maxdeul@gmail.com>
2024-01-07 14:04:57 +0900

    [add]routes.rb及びapplication.rbに追記

--- back/config/application.rb
+++ back/config/application.rb
@@ -14,11 +14,15 @@ require "action_view/railtie"
 require "action_cable/engine"
 # require "rails/test_unit/railtie"
 
+require_relative "boot"
+
+require "rails/all"
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module App
+module Api
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
@@ -37,7 +41,10 @@ module App
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.session_store :cookie_store, key: '_interslice_session'
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
+    config.middleware.use config.session_store, config.session_options
   end
 end
--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,13 +1,22 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
   namespace :api do
     namespace :v1 do
-      mount_devise_token_auth_for 'User', at: 'auth'
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
     end
   end
+
   namespace :current do
     resource :user, only: [:show]
   end
+
+  namespace :auth do
+    resources :sessions, only: [:index]
+  end
+
   resources :posts
   post 'auth/:provider/callback', to: 'api/v1/users#create'
 end
