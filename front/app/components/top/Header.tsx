cf685914ea197f3bc6e8b36a337fe683078002f6
rika <maxdeul@gmail.com>
2024-01-15 03:31:43 +0900

    [change]ログイン方法をGoogleログインのみに変更

--- front/app/components/top/Header.tsx
+++ front/app/components/top/Header.tsx
@@ -2,13 +2,13 @@
 import React from 'react'
 import Link from 'next/link'
 import { useSession } from 'next-auth/react';
-import Image from 'next/image';
-import { Icons } from "@/components/ui/icons"
+import AuthenticateLink from './AuthenticateLink';
+import Dropdown from './Dropdown';
 
 const Header = () => {
   const { data: session } = useSession();
   return (
-    <header className='bg-background-color text-text-color font-genjyuu' style={{ position: 'fixed', top: 0, width: '100%', zIndex: 1000 }}>
+    <header className='bg-background-color text-text-color font-genjyuu' style={{ top: 0, width: '100%'}}>
       <div className='h-20 w-full border-b-2 border-line-color flex items-center justify-between p-2'>
         <ul className='flex'>
           <li className='p-2 cursor-pointer'>
@@ -16,24 +16,9 @@ const Header = () => {
           </li>
         </ul>
         {session ? (
-          <div className='flex items-center'>
-            <p className='p-2'>ようこそ、{session.user?.name}さん</p>
-            <Image
-              src={session.user?.image ?? '/default-avatar.png'}
-              alt="User Avatar"
-              width={40}
-              height={40}
-              style={{ borderRadius: '50%' }}
-            />
-          </div>
+          <Dropdown />
         ) : (
-          <ul className='flex'>
-            <li className='p-2 cursor-pointer'>
-                <Link href='/login'>
-                  Login
-                </Link>
-            </li>
-          </ul>
+          <AuthenticateLink />
         )}
       </div>
     </header>
--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,41 +1,11 @@
 import React from 'react'
-import Link from 'next/link'
-import CustomButton from '@/components/ui/custom-button';
+import Home from './components/top/Home';
 
-export default function Home() {
+
+export default function Top() {
 	return (
-		<div className='bg-background-color min-h-screen text-text-color text-center'>
-			<p className="text-2xl font-bold text-center justify-between pt-20">
-				何かロゴ入れたい
-			</p>
-			<p className="text-center justify-between p-10">
-				『アプリ名』は韓国コスメに特化した、ユーザーのスキンケアをサポートするサービスです。<br />
-			</p>
-			<p className="text-xl font-bold text-center justify-between pt-10">
-				＼ たった1分 ／</p>
-			<p className="text-xl font-bold text-center justify-between pb-5">
-				あなたに合った韓国コスメを探そう！</p>
-			<Link href='/first_demonstration'>
-				<div className="flex justify-center">
-					<CustomButton colorClass="btn-506D7D">診断してみる</CustomButton>
-				</div>
-			</Link>
-			<p className="text-2xl font-bold text-center justify-between pt-10 pb-10">
-				『アプリ名』でできること
-			</p>
-			<ul className="text-center">
-				<li>韓国コスメを使用したことがないけれど興味がある方に向けて、お悩みや肌質別に韓国コスメを提案します。</li>
-				<li>スキンケアコスメの買い忘れ・使用期限切れを防ぐサポートをします。</li>
-				<li>お肌の大敵である紫外線や乾燥から、ユーザーのお肌を守るサポートをします。</li>
-			</ul>
-			<p className="text-2xl font-bold text-center justify-between p-10">
-				なんで韓国コスメは人気なの？
-			</p>
-			<ul className="text-center pb-10">
-				<li>人気の理由書く</li>
-				<li>人気の理由書く</li>
-				<li>人気の理由書く</li>
-			</ul>
+		<div>
+			<Home />
 		</div>
 	);
 };
