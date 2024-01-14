862ad09e031fe1b240872d35b190b0f56371fd29
rika <maxdeul@gmail.com>
2024-01-15 03:43:31 +0900

    [fix]不要な記述を削除

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -1,12 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
 import NextAuthProvider from './providers/NextAuth';
 import Header from './components/top/Header';
 import Footer from './components/top/Footer';
 import { Toaster } from 'react-hot-toast';
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'k skin app',
