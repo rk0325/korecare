d6d03b3829ba874c08998dba690f286d4c44b54b
rika <maxdeul@gmail.com>
2024-01-17 03:09:39 +0900

    [fix]デプロイエラー対応

--- front/app/components/demo/DemonstrationResult.tsx
+++ front/app/components/demo/DemonstrationResult.tsx
@@ -8,7 +8,7 @@ const DemonstrationResult = () => {
 	return (
 		<div className='bg-background-color min-h-screen text-text-color text-center'>
 			<p className="text-2xl font-bold text-center justify-between p-20">
-				あなたに合う韓国コスメはこちら！
+				あなたに合う韓国コスメはこちら！！！
       </p>
       <Link href='/first_demonstration'>
 			<p className="text-center justify-between">
