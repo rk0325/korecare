f0afe879fb91b3847022f527dc66d8b8e5a5e745
rika <maxdeul@gmail.com>
2024-01-17 04:16:54 +0900

    [fix]デプロイエラー対応

--- front/app/UserInfoProvider.tsx
+++ front/app/UserInfoProvider.tsx
@@ -1,6 +1,10 @@
 'use client';
 import React, { useState } from 'react';
 import UserInfoContext from '../contexts/UserInfoContext';
+import FirstDemonstration from './components/demo/FirstDemonstration';
+import SecondDemonstration from './components/demo/SecondDemonstration';
+import ThirdDemonstration from './components/demo/ThirdDemonstration';
+import DemonstrationResult from './components/demo/DemonstrationResult';
 
 function App() {
   const [skinType, setSkinType] = useState("");
@@ -9,9 +13,12 @@ function App() {
 
   return (
     <UserInfoContext.Provider value={{ skinType, setSkinType, age, setAge, skinTrouble, setSkinTrouble }}>
-      {}
+      <FirstDemonstration />
+      <SecondDemonstration />
+      <ThirdDemonstration />
+      <DemonstrationResult />
     </UserInfoContext.Provider>
   );
-};
+}
 
 export default App;
