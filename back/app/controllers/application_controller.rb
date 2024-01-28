0abe61211b6eb0658eaea8c7e0bcdc0ba3359cab
rika <maxdeul@gmail.com>
2024-01-29 00:58:18 +0900

    [refactor]一部コードの修正

--- back/app/controllers/application_controller.rb
+++ back/app/controllers/application_controller.rb
@@ -12,46 +12,45 @@ class ApplicationController < ActionController::API
   private
 
   def set_current_user
-      Rails.logger.info "Current session: #{session.inspect}"  # セッション情報をログに出力
+    Rails.logger.info "Current session: #{session.inspect}"  # セッション情報をログに出力
 
-      if request.headers["Authorization"].present?
-        received_access_token = request.headers["Authorization"].split(' ').last
-      else
-        render json: { error: 'Authorization header is missing' }, status: :unauthorized
+    if request.headers["Authorization"].present?
+      received_access_token = request.headers["Authorization"].split(' ').last
+    else
+      render json: { error: 'Authorization header is missing' }, status: :unauthorized
+      return
+    end
+
+    if session[:user_id] && session[:access_token] == received_access_token
+      # セッションからユーザー情報を取得
+      @current_user = User.find_by(id: session[:user_id])
+    else
+      # Google APIからユーザー情報を取得
+      session.delete(:access_token)
+      user_info = fetch_user_info_from_google(received_access_token)
+
+      if user_info['sub'].nil?
+        render json: { error: 'Failed to fetch user info from Google' }, status: :internal_server_error
         return
       end
 
-      if session[:user_id] && session[:access_token] == received_access_token
-        # セッションからユーザー情報を取得
-        @current_user = User.find_by(id: session[:user_id])
-      else
-        # Google APIからユーザー情報を取得
-        session.delete(:access_token)
-        user_info = fetch_user_info_from_google(received_access_token)
+      # Googleのuidをもとにユーザー検索
+      @current_user = User.find_by(uid: user_info['sub'])
 
-        if user_info['sub'].nil?
-          render json: { error: 'Failed to fetch user info from Google' }, status: :internal_server_error
-          return
-        end
-
-        # Googleのuidをもとにユーザー検索
-        @current_user = User.find_by(uid: user_info['sub'])
-
-        # ユーザーが見つからなかった場合、新規ユーザーを作成
-        if @current_user.nil?
-          @current_user = User.create(
-            provider: 'google',
-            uid: user_info['sub'],
-            name: user_info['name'],
-            email: user_info['email']
-          )
-        end
-
-        # セッションにユーザー情報を保存
-        session[:user_id] = @current_user.id
-        session[:access_token] = received_access_token
+      # ユーザーが見つからなかった場合、新規ユーザーを作成
+      if @current_user.nil?
+        @current_user = User.create(
+          provider: 'google',
+          uid: user_info['sub'],
+          name: user_info['name'],
+          email: user_info['email']
+        )
       end
-      render json: { error: 'Internal Server Error' }, status: :internal_server_error
+
+      # セッションにユーザー情報を保存
+      session[:user_id] = @current_user.id
+      session[:access_token] = received_access_token
+    end
   end
 
   # Googleのユーザー情報を取得
--- back/config/routes.rb
+++ back/config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
     namespace :v1 do
       post 'cosmetics_recommendation/search_cosmetics_for_guests', to: 'cosmetics_recommendation#search_cosmetics_for_guests'
       post 'cosmetics_recommendation/search_cosmetics_for_logged_in_users', to: 'cosmetics_recommendation#search_cosmetics_for_logged_in_users'
-      resources :profiles, only: [:show, :update]
+      resources :profiles, only: [:update]
+      get '/profiles', to: 'profiles#show'
       resources :favorite_cosmetics, only: [:index, :create, :destroy]
     end
   end
