71057a8088d18eaec0dd7b3f9c8071a6323a5635
rika <maxdeul@gmail.com>
2024-02-11 01:11:03 +0900

    [add]ロゴ画像アップロード

--- front/app/components/top/About.tsx
+++ front/app/components/top/About.tsx
@@ -1,20 +1,27 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image';
 import CustomButton from '@/components/ui/custom-button';
 
 const About = () => {
   return (
     <div className='bg-background-color min-h-screen text-text-color text-center font-genjyuu'>
-      <p className="text-2xl font-bold text-center justify-between pt-20">
-        ロゴ
-      </p>
+      <div className="flex justify-center pt-6">
+        <Image
+          src="/Logo.png"
+          alt="LOGO"
+          width={400}
+          height={400}
+          priority
+        />
+      </div>
       <p className="text-center justify-between p-10">
         『KoreCare（コレケア）』は韓国コスメに特化した、毎日のスキンケアをサポートするサービスです。<br />
       </p>
       <p className="text-xl text-center justify-between pt-10">
         2つの質問に答えて</p>
       <p className="text-xl text-center justify-between pb-5">
-      あなたに合った韓国コスメを探そう</p>
+        あなたに合った韓国コスメを探そう</p>
       <Link href='/first_demonstration'>
         <div className="flex justify-center pb-5">
           <CustomButton colorClass="btn-506D7D">試してみる</CustomButton>
