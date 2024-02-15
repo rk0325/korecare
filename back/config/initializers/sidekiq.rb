ee5aad3fd5069f82ef74c63b2ff5511aba059863
rika <maxdeul@gmail.com>
2024-02-16 01:28:28 +0900

    [add]Redisのタイムアウト設定を調整

--- back/config/initializers/sidekiq.rb
+++ back/config/initializers/sidekiq.rb
@@ -3,7 +3,12 @@ require 'sidekiq-cron'
 
 Sidekiq.configure_server do |config|
   schedule_file = "config/schedule.yml"
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = {
+    url: ENV['REDIS_URL'],
+    connect_timeout: 5,
+    read_timeout: 5,
+    write_timeout: 5
+  }
 
   if File.exist?(schedule_file)
     Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
@@ -11,5 +16,10 @@ Sidekiq.configure_server do |config|
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  config.redis = {
+    url: ENV['REDIS_URL'],
+    connect_timeout: 5,
+    read_timeout: 5,
+    write_timeout: 5
+  }
 end
