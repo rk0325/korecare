b05a6b5cd2bc435b60a427f462556a6b60948223
rika <maxdeul@gmail.com>
2024-02-21 10:37:33 +0900

    [update]レイアウト調整

---
+++ back/db/migrate/20240220221720_allow_null_in_users.rb
@@ -0,0 +1,8 @@
+class AllowNullInUsers < ActiveRecord::Migration[7.0]
+  def change
+    change_column_null :users, :provider, true
+    change_column_null :users, :uid, true
+    change_column_null :users, :name, true
+    change_column_null :users, :email, true
+  end
+end
