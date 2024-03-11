600ac84cd442159284c73b52cfa13633a65750cc
rika <maxdeul@gmail.com>
2024-03-12 08:48:57 +0900

    [fix]デプロイエラー対応

--- back/db/migrate/20240119201027_rename_preferences_to_skin_information.rb
+++
@@ -1,5 +0,0 @@
-class RenamePreferencesToSkinInformation < ActiveRecord::Migration[7.0]
-  def change
-    rename_table :preferences, :skin_information
-  end
-end
--- back/db/migrate/20240211055421_add_line_id_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddLineIdToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :line_id, :string, unique: true
-    add_index :users, :line_id, unique: true
-  end
-end
