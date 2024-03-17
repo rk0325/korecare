d14cb5f68b2f44ca6c1477e37b55cace07b44815
rika <maxdeul@gmail.com>
2024-03-17 14:39:03 +0900

    [update]README修正

--- back/app/models/user.rb
+++ back/app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   has_one :profile
   has_many :favorite_cosmetics
   has_many :cosmetic_usages, dependent: :destroy
+  has_many :reviews
+
+  validates :uid, uniqueness: { scope: :provider }
 end
