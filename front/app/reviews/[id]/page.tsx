f6148836141c747f54e0840f4f7f76d941e50388
rika <maxdeul@gmail.com>
2024-03-26 17:38:54 +0900

    [fix]レビュー編集及び削除機能の不具合解消

--- front/app/reviews/[id]/page.tsx
+++ front/app/reviews/[id]/page.tsx
@@ -134,6 +134,7 @@ export default function ReviewDetails() {
       await axios.delete(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/reviews/${reviewId}`, {
         headers: headers,
         withCredentials: true,
+        data: { userId: session?.user?.id }
       });
       setReviews(reviews.filter(review => review.id !== reviewId));
       router.push('/reviews');
